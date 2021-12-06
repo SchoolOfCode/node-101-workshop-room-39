aaf41059ee7d7df77438906c11b816f545bda4bc
tommiesten <94461231+tommiesten@users.noreply.github.com>
2021-12-06 16:54:57 +0000

    prettier being annoying

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 let myCollection = [
-    {
-        name: "School of Code mug",
-        count: 1,
-        whatILike: "It has my cute pixel character on it!",
-    },
-    {
-        name: "School of Code hat",
-        count: 2,
-        whatILike: "An often overlooked fashion accessory",
-    },
-    {
-        name: "School of Code pillow",
-        count: 1,
-        whatILike: "Eat. Sleep. Code. Repeat :)",
-    },
+  {
+    name: "School of Code mug",
+    count: 1,
+    whatILike: "It has my cute pixel character on it!",
+  },
+  {
+    name: "School of Code hat",
+    count: 2,
+    whatILike: "An often overlooked fashion accessory",
+  },
+  {
+    name: "School of Code pillow",
+    count: 1,
+    whatILike: "Eat. Sleep. Code. Repeat :)",
+  },
 ];
 
 // console.log(myCollection);
 
 const describeItem = (count) => {
-    // item = myCollection.count;
-    console.log(
-        `I have ${count} ${myCollection[0].name}s. Here's what I like about it: ${myCollection[0].whatILike}`
-    );
+  // item = myCollection.count;
+  console.log(
+    `I have ${count} ${myCollection[0].name}s. Here's what I like about it: ${myCollection[0].whatILike}`
+  );
 };
 
 describeItem(9);
+
+function describeCollection(item) {
+  myCollection.forEach((item) => console.log(item));
+}
+describeCollection();
