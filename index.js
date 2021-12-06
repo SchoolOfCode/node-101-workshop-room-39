7bda869ad22744a85efac442fe8b0071e71f2084
Corneliu Laur <cl179971@gmail.com>
2021-12-06 21:47:13 +0000

    added the describeItem's function value to be read from the object's count key

--- index.js
+++ index.js
@@ -1,33 +1,37 @@
 let myCollection = [
-  {
-    name: "School of Code mug",
-    count: 1,
-    whatILike: "It has my cute pixel character on it!",
-  },
-  {
-    name: "School of Code hat",
-    count: 2,
-    whatILike: "An often overlooked fashion accessory",
-  },
-  {
-    name: "School of Code pillow",
-    count: 1,
-    whatILike: "Eat. Sleep. Code. Repeat :)",
-  },
+    {
+        name: "School of Code mug",
+        count: 1,
+        whatILike: "It has my cute pixel character on it!",
+    },
+    {
+        name: "School of Code hat",
+        count: 2,
+        whatILike: "An often overlooked fashion accessory",
+    },
+    {
+        name: "School of Code pillow",
+        count: 1,
+        whatILike: "Eat. Sleep. Code. Repeat :)",
+    },
 ];
 
 // console.log(myCollection);
 
 const describeItem = (count) => {
-  // item = myCollection.count;
-  console.log(
-    `I have ${count} ${myCollection[0].name}s. Here's what I like about it: ${myCollection[0].whatILike}`
-  );
+    // Extract the number according to the count key within the object
+    console.log(
+        `I have ${myCollection[1].count} ${myCollection[0].name}s. Here's what I like about it: ${myCollection[0].whatILike}`
+    );
+    // Extract the number according to the function's count parameter/argument
+    console.log(
+        `I have ${count} ${myCollection[0].name}s. Here's what I like about it: ${myCollection[0].whatILike}`
+    );
 };
 
 describeItem(9);
 
 function describeCollection(item) {
-  myCollection.forEach((item) => console.log(item));
+    myCollection.forEach((item) => console.log(item));
 }
-describeCollection();
+describeCollection(3);
