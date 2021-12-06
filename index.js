ae4c07cb048dcdc6981c82d86a1afd59ccfb68ab
Corneliu Laur <cl179971@gmail.com>
2021-12-06 16:39:45 +0000

    task 1 and half 2 complete

---
+++ index.js
@@ -0,0 +1,28 @@
+let myCollection = [
+    {
+        name: "School of Code mug",
+        count: 1,
+        whatILike: "It has my cute pixel character on it!",
+    },
+    {
+        name: "School of Code hat",
+        count: 2,
+        whatILike: "An often overlooked fashion accessory",
+    },
+    {
+        name: "School of Code pillow",
+        count: 1,
+        whatILike: "Eat. Sleep. Code. Repeat :)",
+    },
+];
+
+// console.log(myCollection);
+
+const describeItem = (count) => {
+    // item = myCollection.count;
+    console.log(
+        `I have ${count} ${myCollection[0].name}s. Here's what I like about it: ${myCollection[0].whatILike}`
+    );
+};
+
+describeItem(9);
